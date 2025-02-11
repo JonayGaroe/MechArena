0f3fae73bc276921d5c78753b0ae06e7c0216d6f
JonayGaroe <jonaygaroe@gmail.com>
2025-02-11 13:11:02 +0000

    Clase 11/10
    Añadi persecusion y patrulla ajustando al nivel del robot y que no traspase tambien añadi nuevos collider y arregle script intente añadrile animaciones

--- Assets/EnemigosScript/EstadoPatrulla.cs
+++ Assets/EnemigosScript/EstadoPatrulla.cs
@@ -4,15 +4,116 @@ using UnityEngine;
 
 public class EstadoPatrulla : MonoBehaviour
 {
+
+    public Transform[] wayPoint;    
+
+    private ControladorNavMesh controladorNavMesh;
+
+    private int siguientewayPoint;
+
+    private MaquinaDeEstado maquinadeEstados;
+
+    private ControladorVision controladorVision;
+
+    //
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
+        controladorNavMesh = GetComponent<ControladorNavMesh>();
+        maquinadeEstados = GetComponent<MaquinaDeEstado>();
+        controladorVision = GetComponent<ControladorVision>();
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        RaycastHit hit;
+        if(controladorVision.PuedeVerAlJugador(out hit))
+        {
+
+            controladorNavMesh.perseguirObjetivo = hit.transform;
+
+            maquinadeEstados.ActivarEstado(maquinadeEstados.EstadoPersecuion);
+
+            return;
+                    
+        }   
+
+
+
+
+        if(controladorNavMesh.HemosLlegado())
+        {
+
+
+          siguientewayPoint = (siguientewayPoint + 1) % wayPoint.Length;
+            ActualizaWayPointDestino();
+
+
+        }
+
+         
+
+
+
     }
+
+    private void OnEnable()
+    {
+
+        //siguientewayPoint = 0;
+        ActualizaWayPointDestino();
+
+
+
+    }
+
+    void ActualizaWayPointDestino()
+    {
+
+
+        controladorNavMesh.ActualizarPuntoDestinoNavMeshAgent(wayPoint[siguientewayPoint].position);
+
+
+
+
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+
+        if(other.gameObject.CompareTag("Player"))
+        {
+
+
+
+            maquinadeEstados.ActivarEstado(maquinadeEstados.EstadoAlerta);
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
 }

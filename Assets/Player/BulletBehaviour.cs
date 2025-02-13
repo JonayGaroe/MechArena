1064bfffcec3604896fba95aa9e8cd900d0d4923
JonayGaroe <jonaygaroe@gmail.com>
2025-02-13 13:52:34 +0000

    Cambios en script clase 13-02

---
+++ Assets/Player/BulletBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+
+    public float lifeTime = 2f; // Tiempo antes de regresar al pool
+    public float speed = 20f;
+
+    void OnEnable()
+    {
+        Invoke("ReturnToPool", lifeTime);
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            ReturnToPool();
+        }
+    }
+
+    void ReturnToPool()
+    {
+        CancelInvoke();
+        GenericPool.instance.ReturnProjectile(gameObject);
+    }
+
+
+}
+    
---
+++ Assets/Player/PlayerShootAbility.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootAbility : MonoBehaviour
+{
+
+    public Transform cañonIzquierdo;  // Punto de disparo del cañón izquierdo
+    public Transform cañonDerecho;   // Punto de disparo del cañón derecho
+    public float projectileSpeed = 20f;
+    public float tiempoRecarga = 1f; // Tiempo entre disparos
+    private bool puedeDisparar = true;
+
+    public Animator animator; // Referencia a la animación de disparo
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && puedeDisparar)
+        {
+            StartCoroutine(Disparar());
+        }
+    }
+
+    IEnumerator Disparar()
+    {
+        puedeDisparar = false;
+
+        // Activar animación de disparo
+        if (animator != null)
+        {
+            animator.SetTrigger("Disparar");
+        }
+
+        // Obtener proyectiles del pool
+        GameObject proyectilIzq = GenericPool.instance.GetProjectile();
+        GameObject proyectilDer = GenericPool.instance.GetProjectile();
+
+        if (proyectilIzq != null && proyectilDer != null)
+        {
+            // Posicionar los proyectiles en los cañones
+            proyectilIzq.transform.position = cañonIzquierdo.position;
+            proyectilIzq.transform.rotation = cañonIzquierdo.rotation;
+
+            proyectilDer.transform.position = cañonDerecho.position;
+            proyectilDer.transform.rotation = cañonDerecho.rotation;
+
+            // Asegurar que los proyectiles disparan hacia adelante
+            proyectilIzq.GetComponent<Rigidbody>().velocity = cañonIzquierdo.transform.forward * projectileSpeed;
+            proyectilDer.GetComponent<Rigidbody>().velocity = cañonDerecho.transform.forward * projectileSpeed;
+        }
+
+        // Esperar el tiempo de recarga antes de permitir otro disparo
+        yield return new WaitForSeconds(tiempoRecarga);
+        puedeDisparar = true;
+    }
+}
